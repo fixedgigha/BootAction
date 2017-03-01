fa71b685a8ff9eb1055bd59f6e0b198f4180475f
fixedgigha <Numan123>
2017-03-01 11:55:42 +0000

    Making Geb test Spock-ed

--- src/test/groovy/readinglist/SpringGebTest.groovy
+++ src/test/groovy/readinglist/SpringGebTest.groovy
@@ -1,14 +1,9 @@
 package readinglist
 
-import geb.Browser
 import geb.Page
 import geb.spock.GebSpec
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
 class ReadListPage extends Page {
     static at = { $('h2').text() == 'Your Reading List' }
@@ -21,32 +16,36 @@ class ReadListPage extends Page {
         author { $('input', type: 'text', name: 'author') }
         isbn { $('input', type: 'text', name: 'isbn') }
         description { $('textarea', name: 'description') }
+        listedBook {x -> $('div').$('dl').$('dt.bookHeadline', x) }
     }
 }
 
-@RunWith(SpringRunner)
 @SpringBootTest(classes=ReadingListApplication,
         properties = ['spring.datasource.platform=h2'],
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SpringGebTest {
+class SpringGebTest extends GebSpec {
 
 
     @Value('${local.server.port}')
     private int port
 
-    @Test
-    void testStartApplication() {
-        Browser.drive(driver: new HtmlUnitDriver(), baseUrl: "http://localhost:${port}/") {
+    def setup() {
+        browser.baseUrl = "http://localhost:${port}/"
+    }
+
+    def 'Submit a book and verify displayed' () {
+        given:
             to ReadListPage
             assert at(ReadListPage)
             bookTitle.value('Total Garbage')
             author.value('Jim Bob')
             isbn.value('987654321')
             description.value('Do not buy this\nUnder any circumstances')
+        when:
             submitButton.click()
-            assert at(ReadListPage)
-            assert $('div').$('dl').$('dt.bookHeadline').text() == 'Total Garbage by Jim Bob (ISBN: 987654321)'
-        }
+        then:
+            at(ReadListPage)
+            listedBook(0).text() == 'Total Garbage by Jim Bob (ISBN: 987654321)'
     }
 }
 
